0db8141932ef426a5902fa1fa1c98733becc7c78
markmobe <153935790+markmobe@users.noreply.github.com>
2025-06-13 14:58:56 -0500

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -5,5 +5,8 @@ public class HelloCydeo {
     public static void main(String[] args) {
 
         System.out.println("Hello Cydeo");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello Cydeo");
+
     }
 }
