3fb5954c3fadb77d173600c9dfda494040ed39d4
markmobe <markmobe.23@gmail.com>
2025-06-13 15:36:33 -0500

    BookOfGeez Updated.

--- src/day01/BookOfGeez.java
+++ src/day01/BookOfGeez.java
@@ -4,8 +4,9 @@ public class BookOfGeez {
     public static void main(String[] args) {
 
         System.out.println("A BE GE DE HA WE YE");
-        //A means is  = alfa egziabher.
+        //A means is = alfa egziabher.
         // BE means is bet.
-        // GE means is Gemel/camel .
+        // GE means is Gemel/camel.
+        //HA means is nefs or soul.
     }
 }
